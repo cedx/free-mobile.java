5320406421000e1598923e6e7aa3b610d2fa973b
Cédric Belin <cedric@belin.io>
2023-07-30 15:32:43 +0200

    Add the `ClientException` class

--- src/io/belin/free_mobile/ClientException.java
+++ src/io/belin/free_mobile/ClientException.java
@@ -1,6 +1,26 @@
 package io.belin.free_mobile;
 
+import java.io.IOException;
+
 /**
  * An exception caused by an error in a {@link Client} request.
  */
-class ClientException extends RuntimeException {}
+class ClientException extends IOException {
+
+	/**
+	 * Creates a new client exception.
+	 * @param message The error message.
+	 */
+	public ClientException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Creates a new client exception.
+	 * @param message The error message.
+	 * @param cause The original cause of the error.
+	 */
+	public ClientException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
