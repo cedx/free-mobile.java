02555e74b6dc21ca3db23d878c168cbe5e4e75ca
Cédric Belin <cedric@belin.io>
2024-01-24 10:29:15 +0100

    Update the tests [skip ci]

--- test/io/belin/free_mobile/ClientTest.java
+++ test/io/belin/free_mobile/ClientTest.java
@@ -16,41 +16,41 @@ final class ClientTest {
 	@DisplayName("sendMessage(): should throw a `Client.Exception` if a network error occurred.")
 	void networkError() {
 		var client = new Client("anonymous", "secret", "http://localhost:10000");
-		assertThrows(Client.Exception.class, () -> client.sendMessage("[Sync] Hello World!"));
+		assertThrows(Client.Exception.class, () -> client.sendMessage("Hello World! [sync]"));
 	}
 
 	@Test
 	@DisplayName("sendMessageAsync(): should complete exceptionnaly with a `Client.Exception` if a network error occurred.")
 	void networkErrorAsync() {
 		var client = new Client("anonymous", "secret", "http://localhost:10000");
-		assertThrows(ExecutionException.class, () -> client.sendMessageAsync("[Async] Hello World!").get());
+		assertThrows(ExecutionException.class, () -> client.sendMessageAsync("Hello World! [async]").get());
 	}
 
 	@Test
 	@DisplayName("sendMessage(): should throw a `Client.Exception` if the credentials are invalid.")
 	void invalidCredentials() {
 		var client = new Client("anonymous", "secret");
-		assertThrows(Client.Exception.class, () -> client.sendMessage("[Sync] Hello World!"));
+		assertThrows(Client.Exception.class, () -> client.sendMessage("Hello World! [sync]"));
 	}
 
 	@Test
 	@DisplayName("sendMessageAsync(): should complete exceptionnaly with a `Client.Exception` if the credentials are invalid.")
 	void invalidCredentialsAsync() {
 		var client = new Client("anonymous", "secret");
-		assertThrows(ExecutionException.class, () -> client.sendMessageAsync("[Async] Hello World!").get());
+		assertThrows(ExecutionException.class, () -> client.sendMessageAsync("Hello World! [async]").get());
 	}
 
 	@Test
 	@DisplayName("sendMessage(): should send SMS messages if the credentials are valid.")
 	void validCredentials() {
 		var client = new Client(System.getenv("FREEMOBILE_ACCOUNT"), System.getenv("FREEMOBILE_API_KEY"));
-		assertDoesNotThrow(() -> client.sendMessage("[Sync] Hello Cédric, from Java!"));
+		assertDoesNotThrow(() -> client.sendMessage("Hello Cédric, from Java! [sync]"));
 	}
 
 	@Test
 	@DisplayName("sendMessageAsync(): should send SMS messages if the credentials are valid.")
 	void validCredentialsAsync() {
 		var client = new Client(System.getenv("FREEMOBILE_ACCOUNT"), System.getenv("FREEMOBILE_API_KEY"));
-		assertDoesNotThrow(() -> client.sendMessageAsync("[Async] Hello Cédric, from Java!").get());
+		assertDoesNotThrow(() -> client.sendMessageAsync("Hello Cédric, from Java! [async]").get());
 	}
 }
